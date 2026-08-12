55a914da3dc3b4e448f18268962cd772730964d6
LukeKarger <karge059@umn.edu>
2026-08-11 23:06:03 -0500

    Add user auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Limelight } from "next/font/google";
 import "./globals.css";
-import Link from 'next/link';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "MoviesManaged",
@@ -25,16 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <nav>
-          <ul className="grid grid-cols-3 items-start text-center">
-            <li className="p-3 m-5 text-4xl hover:underline text-left"><Link href="/..">Plan to Watch</Link></li>
-            <h1 className="p-3 m-5 text-4xl hover:underline">Movies Managed</h1>
-            <li className="p-3 m-5 text-4xl hover:underline text-right"><Link href="/watched">Watched</Link></li>
-          </ul>
-        </nav>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
